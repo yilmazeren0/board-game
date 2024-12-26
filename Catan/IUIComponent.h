d792e8af3e2fd341d55115b7a23b66b89480a005
yilmazeren0 <errreny16@gmail.com>
2024-12-27 00:35:51 +0300

    unnecesarry files deleted

--- Catan/IUIComponent.h
+++
@@ -1,9 +0,0 @@
-#include <SFML/Graphics.hpp>
-
-class IUIComponent {
-public:
-    virtual ~IUIComponent() = default;
-    virtual void draw(sf::RenderWindow& window) = 0;
-    virtual void update(sf::Vector2f mousePosition) = 0;
-    virtual bool isHovered() const = 0;
-};
